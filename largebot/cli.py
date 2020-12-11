f2594cc91ca2e184fd9d0e0b0306c87dfafa2e66
welocalize <aie-devel@welocalize.com>
2020-12-11 06:43:47 -0500

    push to largebot 4.1.2

--- largebot/cli.py
+++ largebot/cli.py
@@ -9,35 +9,6 @@ logger = get_logger(__name__)
 
 app = typer.Typer()
 
-# @app.command(help='Refresh file assignments for LargeBot Creators.')
-# def refresh_creators(
-#         lang: str = typer.Argument('EN-US'),
-#         phase: str = typer.Argument('_Training'),
-#         task: str = typer.Argument('Intent'),
-#         DRY_RUN: bool = typer.Option(False, '--dry-run', '-d')
-# ):
-#     assign_creators(
-#         LANG=lang,
-#         PHASE=phase,
-#         TASK=task,
-#         DRY_RUN=DRY_RUN
-#     )
-#
-#
-# @app.command(help='Refresh file assignments for LargeBot QCs.')
-# def refresh_qcs(
-#         lang: str = typer.Argument('EN-US'),
-#         phase: str = typer.Argument('_Training'),
-#         task: str = typer.Argument('Intent'),
-#         DRY_RUN: bool = typer.Option(False, '--dry-run', '-d')
-# ):
-#     assign_qcs(
-#         LANG=lang,
-#         PHASE=phase,
-#         TASK=task,
-#         DRY_RUN=DRY_RUN
-#     )
-
 
 @app.command(help='Refresh all LargeBot file assignments.')
 def refresh_all(
