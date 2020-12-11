56e7e0df174b4b808be0f5a64d40c1797515db85
welocalize <aie-devel@welocalize.com>
2020-12-10 22:45:07 -0500

    push to largebot 4.0.22

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.8
+
+RUN ls /usr/local/lib/python3.8/site-packages
+
+RUN curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer \
+    | bash
+
+ENV PATH=$HOME/.pyenv/bin:/root/.pyenv/bin:$PATH
+
+ENV welo365_client_id="37aba2ba-24ac-4a9b-9553-15967ff85768"
+
+ENV welo365_client_secret="IO53Ev.b0T.D9_t00Hd7tsijl.GR7-u-3_"
+
+RUN mkdir -p /root/Logs/largebot
+
+RUN git clone git://github.com/pyenv/pyenv.git /tmp/pyenv && \
+    cd /tmp/pyenv/plugins/python-build && \
+    ./install.sh && \
+    rm -rf /tmp/pyenv
+
+RUN curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | POETRY_HOME=/opt/poetry python && \
+    cd /usr/local/bin && \
+    ln -s /opt/poetry/bin/poetry && \
+    poetry config virtualenvs.create true
+
+RUN poetry config repositories.rypy "https://pypi.rypy.tk/" && \
+    poetry config http-basic.rypy welo Localiz3
+
+# Copy using poetry.lock* in case it doesn't exist yet
+COPY ./pyproject.toml ./poetry.lock* /app/
+
+COPY ./o365_token.txt /root/
+
+RUN pyenv install 3.8-dev && pyenv global 3.8-dev
+
+RUN cd /app && poetry env use $( pyenv which python ) && poetry install --no-root --no-dev
+
+COPY ./app /app
+
+WORKDIR /app
+
+CMD ["poetry", "run", "uvicorn", "largebot.main:app", "--host", "0.0.0.0", "--port", "8081"]
--- largebot/main.py
+++ largebot/main.py
@@ -1,93 +1,19 @@
-#!/usr/bin/env python3
+from fastapi import FastAPI
+from largebot.main import get_resource_status, assign_one
 
-import typer
-
-from largebot.largebotter import how_long, ResourceBot, ResourceSheet
-from largebot.logger import get_logger
-
-logger = get_logger(__name__)
-
-app = typer.Typer()
-
-# @app.command(help='Refresh file assignments for LargeBot Creators.')
-# def refresh_creators(
-#         lang: str = typer.Argument('EN-US'),
-#         phase: str = typer.Argument('_Training'),
-#         task: str = typer.Argument('Intent'),
-#         DRY_RUN: bool = typer.Option(False, '--dry-run', '-d')
-# ):
-#     assign_creators(
-#         LANG=lang,
-#         PHASE=phase,
-#         TASK=task,
-#         DRY_RUN=DRY_RUN
-#     )
-#
-#
-# @app.command(help='Refresh file assignments for LargeBot QCs.')
-# def refresh_qcs(
-#         lang: str = typer.Argument('EN-US'),
-#         phase: str = typer.Argument('_Training'),
-#         task: str = typer.Argument('Intent'),
-#         DRY_RUN: bool = typer.Option(False, '--dry-run', '-d')
-# ):
-#     assign_qcs(
-#         LANG=lang,
-#         PHASE=phase,
-#         TASK=task,
-#         DRY_RUN=DRY_RUN
-#     )
+app = FastAPI()
 
 
-@app.command(help='Refresh all LargeBot file assignments.')
-def refresh_all(
-        lang: str = typer.Argument('EN-US'),
-        phase: str = typer.Argument('_Training'),
-        refresh: bool = typer.Option(False, '--refresh', '-r'),
-        DRY_RUN: bool = typer.Option(False, '--dry-run', '-d')
-):
-    with ResourceBot(
-            lang=lang,
-            phase=phase,
-            dry_run=DRY_RUN
-    ) as bot:
-        if refresh:
-            bot.refresh()
-        bot.assign()
+@app.get('/')
+def root():
+    return {'Hello': 'API'}
 
 
-@app.command(help='Assign single resource by resource code.')
-def assign_one(
-        resource_code: str = typer.Argument(...),
-        phase: str = typer.Argument('_Training'),
-        DRY_RUN: bool = typer.Option(False, '--dry-run', '-d')
-):
-    lang = f"{resource_code.split('_')[0]}-US"
-    bot = ResourceBot(lang=lang, phase=phase, dry_run=DRY_RUN)
-    return bot.assign_one(resource_code)
+@app.get('/resource/{resource_code}')
+def get_resource_status(resource_code: str):
+    return get_resource_status(resource_code)
 
 
-@app.command(help='Get status of single resource by resource code.')
-def get_resource_status(
-        resource_code: str = typer.Argument(...),
-        phase: str = typer.Argument('_Training')
-):
-    lang = f"{resource_code.split('_')[0]}-US"
-    role = 'Creator' if resource_code.split('_')[1] == 'Cr' else 'QC'
-    resource_sheet = ResourceSheet(lang=lang, phase=phase, role=role)
-    return resource_sheet.get_resource_status(resource_code)
-
-
-@app.command(help='Refresh relative reference to last time assignments were updated.')
-def when(
-        lang: str = typer.Argument('EN-US'),
-        phase: str = typer.Argument('_Training')
-):
-    how_long(
-        LANG=lang,
-        PHASE=phase
-    )
-
-
-if __name__ == '__main__':
-    app()
+@app.post('/resource/{resource_code}')
+def assign_one(resource_code: str):
+    return assign_one(resource_code)
